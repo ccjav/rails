4ad2a70478d1530dd6a5d31a6ff9aa060f36f753
jja <gitter@tutanota.com>
2017-03-15 20:16:17 -0600

    post its avances

--- post_its_board/post-it.js
+++ post_its_board/post-it.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {  
+$( document ).ready(function() {
   var allBoards = {};
   $(function() {
     $( "#newBoardForm" ).submit(function( event ) {
@@ -11,40 +11,56 @@ $( document ).ready(function() {
     });
     // Esta es la fucnión que correrá cuando este listo el DOM
   });
-});
 
-var Board = function( selector ) {
-  // Aqui denerá ir el código que tenga que ver con tu tablero 
+  var Board = function( selector ) {
+  // Aqui denerá ir el código que tenga que ver con tu tablero
   this.id = selector;
-  this.htmlCode = '<div id=\"' + selector + '\"' + 
+  this.htmlCode = '<div id=\"' + selector + '\"' +
           ' style="background-color: #9e8d68; margin: 0; padding: 0; position: absolute; height: 100%; width: 90%; right: 0; top: 0;">'+
           '</div>';
+
+  this.postIt = 'div id="postit"></div>'
   // $(elm).appendTo("body");
   // // $( "#menu" ).append("<p>" + boardName + "</p>");
   // // Utiliza esta sintaxis para referirte al selector que representa al tablero.
-  // // De esta manera no dependerás tanto de tu HTML.  
+  // // De esta manera no dependerás tanto de tu HTML.
   // var $elem = $( selector );
   // console.log("este es all boards 2 ");
   //     console.log(allBoards);
   // // $( "body" ).append("<div id=" + $elem.selector + "></div>")
 
-  
+
 };
 
 Board.prototype.createBoard = function() {
+  g = document.createElement('div');
+  g.setAttribute("id", this.id);
+  g.className = "board";
+  // var currentBoard = $("<div></div>");
+  // $g.css({"background-color" : "#9e8d68", "position" : "absolute", "height" : "100%", "width" : "90%", "right" : 0, "top" : 0});
+  document.getElementsByTagName('body')[0].appendChild(g);
+
+
+  // var currentBoard = $('body').find(this.id)
+  // console.log(currentBoard)
+  // $(currentBoard).css({"background-color" : "#9e8d68", "position" : "absolute", "height" : "100%", "width" : "90%", "right" : 0, "top" : 0})
+  console.log(this.id)
+  console.log("en createBoard padre")
+  // var $ele = $( 'body' ).find($elem);
+  console.log($($elem));
   var $elem = this.id;
   $( 'body' ).on( "dblclick", $($elem), function(e) {
     // if (e.target.id == $elem) {
-      var newPostIt = new PostIt(); 
+      var newPostIt = new PostIt();
       newPostIt.createPostIt($elem);
-      newPostIt.makeDraggable($elem);
-      newPostIt.closePostIt($elem);
+      // newPostIt.makeDraggable($elem);
+      // newPostIt.closePostIt($elem);
       console.log("doble click");
     // }
   });
   // $( $elem ).dblclick(function(e) {
   //   // if (e.target == $elem[0]) {
-  //   //   var newPostIt = new PostIt(); 
+  //   //   var newPostIt = new PostIt();
   //   //   newPostIt.createPostIt($elem[0]);
   //   //   newPostIt.makeDraggable($elem[0]);
   //   //   newPostIt.closePostIt($elem[0]);
@@ -54,14 +70,6 @@ Board.prototype.createBoard = function() {
 
   // $( this.htmlCode ).appendTo('body');
 
-  $('body').append($('<div/>',{
-    'id'    : 'child'
-  }));
-  // background-color: #9e8d68; margin: 0; padding: 0; position: absolute; height: 100%; width: 90%; right: 0; top: 0;
-  $('#child').css({"background-color" : "#9e8d68", "position" : "absolute", "height" : "100%", "width" : "90%", "right" : 0, "top" : 0})
-  console.log("en createBoard padre")
-  // var $ele = $( 'body' ).find($elem);
-  console.log($($elem));
 }
 
 var PostIt = function() {
@@ -71,22 +79,31 @@ var PostIt = function() {
 
 PostIt.prototype.createPostIt = function(boardSelector) {
   // var $currentBoard = $('body').find(boardSelector);
-  $( "<p>fsdaf</p>" ).appendTo( "#child" );
+  // var g = document.createElement('div');
+  // g.setAttribute("id", "master");
+  // var hola = document.getElementById(boardSelector);
+  // console.log("hola " )
+  // console.log(hola)
+  // hola.appendChild(g);
+  var bola = $(boardSelector);
+  // $( boardSelector ).show();
+  $(bola).append("<p>fsdaf</p>")
+  $( "<p>fsdaf</p>" ).appendTo( bola.selector );
   // $( this.htmlCode ).appendTo( boardSelector ).css({"left" : event.pageX, "top" : event.pageY});
   // $(boardSelector).add("p")
-  $( boardSelector ).html("<p>fsdaf</p>");
+  // $( 'boardSelector' ).html("<p>fsdaf</p>");
 
   // var btn = document.createElement("BUTTON");
   // document.body.appendChild(btn);
   console.log("en create post it");
   console.log(this.htmlCode);
-  console.log(boardSelector);
+  console.log(bola);
 }
 
 PostIt.prototype.makeDraggable = function(boardSelector) {
-  $("#" + boardSelector.id + " > div").draggable({ 
+  $("#" + boardSelector.id + " > div").draggable({
     handle: ".header",
-    start: function( event, ui ) { 
+    start: function( event, ui ) {
       ui.helper.css("z-index", "1");
       ui.helper.siblings().css("z-index", "0");
     }
@@ -98,3 +115,6 @@ PostIt.prototype.closePostIt = function(boardSelector) {
     $(this).parentsUntil("#" + boardSelector.id).remove();
   });
 }
+});
+
+
